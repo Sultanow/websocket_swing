10b131537980a28e5ed71c5d19146411562beb45
esultano <esultano@DESKTOP-J7JQBPN>
2019-10-18 07:41:38 +0200

    added vue app

---
+++ node-vue-client/doc/Readme.txt
@@ -0,0 +1,7 @@
+npm install -g @vue/cli
+
+  (vue add vuetify
+   yarn remove vuetify
+   npm uninstall -S vuetify)
+
+npm run serve
--- swing/src/main/java/websocket/Main.java
+++ swing/src/main/java/websocket/Main.java
@@ -2,10 +2,13 @@ package websocket;
 
 import java.io.IOException;
 import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.concurrent.CountDownLatch;
 
+import javax.websocket.CloseReason;
 import javax.websocket.ContainerProvider;
 import javax.websocket.DeploymentException;
-import javax.websocket.OnError;
+import javax.websocket.OnClose;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
@@ -13,36 +16,74 @@ import javax.websocket.WebSocketContainer;
 
 public class Main {
 
+	// We used CountDownLatch to make sure that main thread does not exit after
+	// executing the code. The main thread waits till the time latch decrements the
+	// counter in onClose() method.
+	private static CountDownLatch latch;
+
 	public static void main(String[] args) throws IOException, DeploymentException {
-		WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+		latch = new CountDownLatch(1);
+		
 		String uri = "ws://localhost:1337/";
 		System.out.println("Connecting to " + uri);
-		container.connectToServer(ClientEndpoint.class, URI.create(uri));
+
+		// open websocket
+		final WebsocketClientEndpoint clientEndPoint = new WebsocketClientEndpoint(URI.create(uri));
+		clientEndPoint.sendMessage("{''}");
 	}
 
 	@javax.websocket.ClientEndpoint
-	class ClientEndpoint {
+	public static class WebsocketClientEndpoint {
 
-		@OnOpen
-		public void onOpen(Session session) {
+		Session userSession = null;
+
+		public WebsocketClientEndpoint(URI endpointURI) {
 			try {
-				String name = "Duke";
-				System.out.println("Sending message to endpoint: " + name);
-				session.getBasicRemote().sendText(name);
-			} catch (IOException ex) {
-				ex.printStackTrace();
-			}
+				WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+				container.connectToServer(this, endpointURI);
+				latch.await();
+			} catch (DeploymentException | IOException | InterruptedException e) {
+	            throw new RuntimeException(e);
+	        }
 		}
 
+		/**
+		 * Callback hook for Connection open events.
+		 *
+		 * @param userSession the userSession which is opened.
+		 */
+		@OnOpen
+		public void onOpen(Session userSession) {
+			System.out.println("opening websocket");
+			this.userSession = userSession;
+		}
+
+		/**
+		 * Callback hook for Connection close events.
+		 *
+		 * @param userSession the userSession which is getting closed.
+		 * @param reason      the reason for connection close
+		 */
+		@OnClose
+		public void onClose(Session userSession, CloseReason reason) {
+			System.out.println("closing websocket");
+			this.userSession = null;
+			latch.countDown();
+		}
+
+		/**
+		 * Callback hook for Message Events. This method will be invoked when a client
+		 * send a message.
+		 *
+		 * @param message The text message
+		 */
 		@OnMessage
-		public void processMessage(String message) {
-			System.out.println("Received message in client: " + message);
-
+		public void onMessage(String message) {
+			System.out.println(message);
 		}
 
-		@OnError
-		public void processError(Throwable t) {
-			t.printStackTrace();
+		public void sendMessage(String message) {
+			this.userSession.getAsyncRemote().sendText(message);
 		}
 	}
 }
